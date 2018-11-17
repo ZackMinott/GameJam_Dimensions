f4460ee6433837d545a9c1736caa24ca9a9dbd7b
DYRIX\Graysire <graysire@dyrix.net>
2018-11-17 02:47:29 -0800

    Made asteroids spin while falling

--- Dimensions Game Jam/Assets/Scripts/EndlessFallingScript.cs
+++ Dimensions Game Jam/Assets/Scripts/EndlessFallingScript.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class EndlessFallingScript : MonoBehaviour {
 
     GameObject background;
+    float spinFactor;
 
 	// Use this for initialization
 	void Start ()
     {
         background = GameObject.Find("Background");
+        spinFactor = Random.Range(1f, 8f);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         transform.Translate(new Vector3(0, -1 * background.GetComponent<BackdropScript>().speed * Time.deltaTime), Space.World);
+        if (!(transform.gameObject.tag == "Coin"))
+        {
+            transform.Rotate(new Vector3(0, 0, spinFactor));
+        }
         if (transform.position.y <= -6)
         {
             Object.Destroy(transform.gameObject);
