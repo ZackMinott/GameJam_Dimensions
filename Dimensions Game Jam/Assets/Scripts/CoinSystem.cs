03782f02a96c642868d75166597cffde854d2f3d
Kim Marzo <kjrmarzo@gmail.com>
2018-11-16 23:40:52 -0500

    Coin system added

---
+++ Dimensions Game Jam/Assets/Scripts/CoinSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSystem : MonoBehaviour {
+   
+    public int coins = 0;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(col.gameObject);
+        if (col.gameObject.tag == "Coin")
+        {
+            coins++;
+
+        }
+        
+    }
+}
