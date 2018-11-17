abfdf4df4715692c3f59032a7bf0f646f95a0d1e
Kim Marzo <kjrmarzo@gmail.com>
2018-11-17 00:44:12 -0500

    Complete Coin System - needs to be integrated to the life system

--- Dimensions Game Jam/Assets/Scripts/CoinSystem.cs
+++ Dimensions Game Jam/Assets/Scripts/CoinSystem.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinSystem : MonoBehaviour {
    
     public int coins = 0;
-    private void OnTriggerEnter2D(Collider2D col)
+    public Text countText;
+
+    void Start()
+    {
+  
+        SetCountText();
+    }
+
+ 
+    void OnTriggerEnter2D(Collider2D col)
     {
         Destroy(col.gameObject);
         if (col.gameObject.tag == "Coin")
         {
             coins++;
-
+            countText.text = "Coins:" + coins;
+            SetCountText();
         }
         
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Coins:" + coins;
     }
 }
---
+++ Dimensions Game Jam/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+   // public GUIText scoreTest;
+	// Use this for initialization
+	void Start () {
+       // scoreTest.text = "Coins:" + coins;
+	}
+    
+}
